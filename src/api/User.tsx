531f1313404ad051682cc0ea7052aba09a706cb9
MOKUDAI Shoki <mokudai.shoki@gmail.com>
2025-10-02 10:35:25 +0900

    バックエンドアドレスのベタ書きを直しました２

--- src/api/User.tsx
+++ src/api/User.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getUser = async (user_id: number, token: string) => {
-	const url = `http://localhost:3001/user/${user_id}?token=${token}`;
+	const url = `${process.env.REACT_APP_BACK_ADRESS}/user/${user_id}?token=${token}`;
 	const res = await axios.get(url);
 	return res.data;
 };
